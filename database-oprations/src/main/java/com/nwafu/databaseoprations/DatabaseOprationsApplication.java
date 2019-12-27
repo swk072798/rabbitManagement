4d7e5b6b0e66a5742845cc8916bf34215d93ce85
swk072798 <474052231@qq.com>
2019-12-27 13:58:43 +0800

    动态数据源配置

--- database-oprations/src/main/java/com/nwafu/databaseoprations/DatabaseOprationsApplication.java
+++ database-oprations/src/main/java/com/nwafu/databaseoprations/DatabaseOprationsApplication.java
@@ -2,8 +2,11 @@ package com.nwafu.databaseoprations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})       //禁用数据源自动配置
+@ComponentScan(basePackages = "com.nwafu.databaseoprations.config")
 public class DatabaseOprationsApplication {
 
     public static void main(String[] args) {
